bf1ace0889a3ef0516422664a9156d7ab96cb1ae
justin152 <justinr@mtcpro.com>
2019-08-30 16:29:04 -0400

    Added name, email validation for user. Added validation that content exists. Added most recent post to user show.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,13 @@
 class User < ApplicationRecord
   has_many :microposts
+  validates :name, presence: true
+  validates :email, presence: true
+
+  def first_post
+    if microposts.any?
+      microposts.order('created_at DESC').first.content
+    else
+      ''
+    end
+  end
 end
